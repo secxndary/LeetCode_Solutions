a49efd84c4c589ef09803275d264ea4861962b9c
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-02-02 16:38:22 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ LeetCode_Solutions/1431-Kids-With-the-Greatest-Number-of-Candies.cs
@@ -0,0 +1,14 @@
+namespace LeetCode_Solutions;
+
+public partial class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        var result = new bool[candies.Length];
+
+        for (var i = 0; i < candies.Length; i++) {
+            if (candies[i] + extraCandies >= candies.Max())
+                result[i] = true;
+        }
+
+        return result;
+    }
+}
