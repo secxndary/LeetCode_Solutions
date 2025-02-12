938ad7bd1715eaa41b1b81b8731efeb82e5512da
alexander valdaitsevv <valdaitsevv@mail.ru>
2025-02-12 11:51:21 +0300

    392. Is Subsequence

---
+++ LeetCode_Solutions/0392-Is-Subsequence.cs
@@ -0,0 +1,22 @@
+namespace LeetCode_Solutions;
+
+public partial class Solution {
+    public bool IsSubsequence_WithTwoCharArrays(string s, string t) {
+        var sChar = s.ToCharArray();
+        var tChar = t.ToCharArray();
+        var index = 0;
+
+        if (string.IsNullOrEmpty(s))
+            return true;
+
+        for (var i = 0 ; i < t.Length; i++) {
+            if (sChar[index] == tChar[i])
+                index++;
+            
+            if (index == s.Length)
+                return true;
+        }
+
+        return false;
+    }
+}
